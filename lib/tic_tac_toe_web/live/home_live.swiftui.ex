9b6e09bccc61d70873b607b7d269b3ea5c7d0c6d
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2025-11-07 00:05:44 -0300

    Add watchos design.

--- lib/tic_tac_toe_web/live/home_live.swiftui.ex
+++ lib/tic_tac_toe_web/live/home_live.swiftui.ex
@@ -5,6 +5,47 @@ defmodule TicTacToeWeb.HomeLive.SwiftUI do
 
   use TicTacToeNative, [:render_component, format: :swiftui]
 
+  def render(assigns, %{"target" => "watchos"}) do
+    assigns = assign(assigns, :rows, Enum.chunk_every(assigns.board, 3))
+
+    ~LVN"""
+    <VStack>
+      <HStack>
+        <%= case @board_state do %>
+          <% :ongoing -> %>
+            <Text>Jogo em progresso</Text>
+          <% {:winner, winner} -> %>
+            <Text>{"#{winner} venceu!"}</Text>
+          <% :tie -> %>
+            <Text>{"Empate!"}</Text>
+        <% end %>
+      </HStack>
+      <HStack>
+        <Button phx-click="restart" style={".buttonStyle(.plain); .controlSize(.small)"}>
+          <Text style={".padding(2); .background(Color.orange);"}>Reiniciar Game</Text>
+        </Button>
+      </HStack>
+      <Grid alignment="center" horizontalSpacing="20" verticalSpacing="20" style=".padding()">
+        <GridRow :for={row <- @rows} alignment="center">
+          <Button
+            :for={{cell_index, cell_value} <- row}
+            id={"cell-#{cell_index}"}
+            phx-click="cell_clicked"
+            phx-value-index={cell_index}
+            style={".buttonStyle(.borderedProminent); .controlSize(.mini);"}
+          >
+            <%= if cell_value do %>
+              <Label>{cell_value}</Label>
+            <% else %>
+              <Label>_</Label>
+            <% end %>
+          </Button>
+        </GridRow>
+      </Grid>
+    </VStack>
+    """
+  end
+
   def render(assigns, _interface) do
     assigns = assign(assigns, :rows, Enum.chunk_every(assigns.board, 3))
 
@@ -30,11 +71,12 @@ defmodule TicTacToeWeb.HomeLive.SwiftUI do
             id={"cell-#{cell_index}"}
             phx-click="cell_clicked"
             phx-value-index={cell_index}
+            style={".buttonStyle(.borderedProminent); .controlSize(.large);"}
           >
             <%= if cell_value do %>
               <Label>{cell_value}</Label>
             <% else %>
-              <Label systemImage="placeholdertext.fill">{cell_value}</Label>
+              <Label>_</Label>
             <% end %>
           </Button>
         </GridRow>
